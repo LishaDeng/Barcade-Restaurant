6ed30b5d111d4bd8d69ab92cc35820d2522026a0
LishaDeng <lishadeng@hotmail.com>
2015-05-03 19:15:25 -0400

    3 group in menu

--- barcade.js
+++ barcade.js
@@ -1,12 +1,45 @@
   var foodList=document.getElementById('foodList');
   var foodButton=document.getElementById('foodButton');
+  var drinkList=document.getElementById('drinkList');
+  var drinkButton=document.getElementById('drinkButton');
+  var specialsList=document.getElementById('specialsList');
+  var specialsButton=document.getElementById('specialsButton');
 
    foodButton.addEventListener("click", function(){
 
     if (foodList.hasAttribute("expanded")){
       foodList.removeAttribute("expanded");
+      drinkList.setAttribute("expanded", "true");
+      specialsList.removeAttribute("expanded");
     } else {
       foodList.setAttribute("expanded", "true");
+      drinkList.removeAttribute("expanded");
+      specialsList.removeAttribute("expanded");
     }
-
   },false);
+
+   drinkButton.addEventListener("click", function(){
+
+     if (drinkList.hasAttribute("expanded")){
+       drinkList.removeAttribute("expanded");
+       foodList.setAttribute("expanded", "true");
+       specialsList.removeAttribute("expanded");
+     } else {
+       drinkList.setAttribute("expanded", "true");
+       foodList.removeAttribute("expanded");
+       specialsList.removeAttribute("expanded");
+     }
+   },false);
+
+   specialsButton.addEventListener("click", function(){
+
+     if (specialsList.hasAttribute("expanded")){
+       specialsList.removeAttribute("expanded");
+       drinkList.setAttribute("expanded", "true");
+       foodList.removeAttribute("expanded");
+     } else {
+       specialsList.setAttribute("expanded", "true");
+       drinkList.removeAttribute("expanded");
+       foodList.removeAttribute("expanded");
+     }
+   },false);
